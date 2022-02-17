c5eaef8b6540580eb65e539f7e3b274ae02d316b
Ricardo Lino <ricardo@costalino.com>
2022-02-17 12:21:13 -0300

    1 passo a menos percorrendo o array

--- selectioSort.js
+++ selectioSort.js
@@ -1,12 +1,16 @@
-const livros = require('./listaLivros').precosLivro;
+var livros = require('./listaLivros').precosLivro;
 const menorValor = require('./funcPrecos').menorValor
+console.log(livros);
 
-for (let atual = 0; atual < livros.length; atual++) {
+for (let atual = 0; atual < livros.length-1; atual++) {
     let menor = menorValor(livros, atual);
-
+    //console.log(`Menor indice = ${menor}`)
     let livroAtual = livros[atual];
+    console.log(`Posicao Atual = ${atual} e livro atual é ${livroAtual.titulo}  de preco ${livroAtual.preco}`)
+    
     let livroMenorPreco = livros[menor];
-
+    console.log(`O livro de menor é ${livroMenorPreco.titulo} de preco ${livroMenorPreco.preco} na posica ${menor}`)
+    
     livros[atual] = livroMenorPreco;
     livros[menor] = livroAtual;
 }
